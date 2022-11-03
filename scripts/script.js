39f113ae04d077c66b08b1367a3f4fd5dd6b1a72
Ahmad Hariri <ahmadmhd357@gmail.com>
2022-11-04 02:28:20 +0300

    solving the first issue

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,6 @@
 const randomWord = ()=>{
     return fetch('https://random-word-api.herokuapp.com/word?number=1') //fetching from the api that generate random word every time
-    .then(res=>res.json()) // turning the result of fetching to json
-    
+    .then(res=>res.json()) // turning the result of fetching to json  
 }
 randomWord().then(x) // I told the brwoser here to wait until the fetching is done then invok the function
 let words;
@@ -13,21 +12,39 @@ function x(random){    // this is the main function
   words = random.toString();       // here I turned the result of fetching to be string
   console.log(words)
   letterarr = words.split('')      // here I made an array of the letters of the string that we got frome the fetching
-  console.log(letterarr)
-  lettersI = letterarr.map((letter,index)=>{       // here I applayed map method on the array of letters
-    
-    let input = document.createElement('div')     // this line to create an element for each letter in the array so if we have word of 5 letters it will make 5 divs
+  // here I applayed map method on the array of letters
+  lettersI = letterarr.map((letter,index)=>{       
+    let input = document.createElement('div')
+    input.setAttribute('id',`letter-${letter}`)     // this line to create an element for each letter in the array so if we have word of 5 letters it will make 5 divs
     input.classList.add('boxes')                  // giving a class for each div I create so we can design easly in the css
-    input.innerHTML = `<span id = letter-${letter}>_ </span>`       //creating to hold the space that will be letter then and give it an Id of the index 
-    document.getElementById('holder').appendChild(input)   // appending the holding element to the divs
-    for(i=0;i<letterarr.length;i++){                // I looped here through the array of letters so I can call only the buttons that true
-     let lett = document.getElementById(letterarr[i]) //here I called the buttons that equal the letters of the word
-     lett.addEventListener('click',e=>{  // here I'm applaying event listener on the buttons
-        e.preventDefault()
-        // here I am trying to change the span inner text to be the letter but I did't succeded yet
-     })
-    }
+    input.innerHTML = `<span id =${index}>_ </span>`       //creating to hold the space that will be letter then and give it an Id of the index 
+    document.getElementById('holder').appendChild(input)  // appending the dives we create to the main div which called holder(check the html to understand better)
   })
+  //after I finished of creating the elements that I will use now I will start working on making events to the letter buttons so if the user clicked the right letter it will appear in the blanks we made for before(spans)
+  let spans = document.querySelectorAll('.boxes span') // getting all the spans that will hold the letters of the word 
+  document.addEventListener('click',e=>{     // this event Listener for all the buttons
+      if(e.target.className === 'letter-btn'){   // this if statmente to make the event listen for the letters buttons only
+        e.target.classList.add('clicked') // adding new class for the buttons that we clicked so we can design them after the clicking
+      let clickedLetter = e.target.value  //getting the value of the clicked letter which is the letter it self
+      letterarr.forEach((letter,letterIndex) => {   // looping in the array of the random word letters to see if the clicked letter is in the word or not
+        
+        if(clickedLetter==letter){          //checking if the clicked letter equal any letter of the array
+        spans.forEach((span,spanIndex)=>{   // if the statment is true I will loop on the array of spans to change the inner text of the span
+          if(spanIndex === letterIndex){    // when the place of the span equal the place of the letter
+            span.innerHTML = clickedLetter  // change this span inner text to be the letter it self
+          }
+        })
+        }
+        
+      });
+      }
+  })
+  //now we finished the clicking letters part and we can move to the next part
+  
+  
+    
+  
+   
 }
 
 
